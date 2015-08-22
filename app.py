59b521b936c3117c1ece8aaa17ad5b1d190df4b0
hernan <hernan@carlas-MacBook-Pro.local>
2015-08-22 09:39:24 -0400

    Add quandl, pull stock price for last 30 days, and pass info to graph_page

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure, output_file, show
+import Quandl
 
 app = Flask(__name__)
+
+app.stock = ''
+app.closing = 0
 
 @app.route('/')
 def main():
   return redirect('/index')
 
-@app.route('/index')
+@app.route('/index',methods=['GET','POST'])
 def index():
-  return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        app.stock = request.form['stock_name']
+    
+        mydata = Quandl.get("WIKI/"+app.stock, rows=30)
+        
+        dates = mydata.Close.index
+        closing_price = mydata.Close.values
+        app.closing = closing_price[0]
+        
+        f= open('check','w')
+        print >>f, app.closing
+        f.close()
+        
+        return redirect('/graph_page') 
+
+@app.route('/graph_page')
+def graph_page():
+    
+    return render_template('graph.html',stock_symbol = app.stock, closing = app.closing)
 
 if __name__ == '__main__':
   app.run(port=33507)
