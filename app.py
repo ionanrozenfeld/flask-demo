5fc1f700576a4803e663aaf89e1b8a2bffb3103e
hernan <hernan@carlas-MacBook-Pro.local>
2015-09-06 19:52:33 -0400

    Add checkboxes, error plage, display of data, and change css

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ import pandas as pd
 
 app = Flask(__name__)
 
-app.stock = ''
 app.script = ''
 app.div = ''
 
@@ -26,24 +25,48 @@ def index():
         app.div = ''
         app.stock_symbol = ''
         app.stock_name = ''
-
+        app.closing_price = ''
+        app.volume = ''
+        
         return render_template('index.html')
     else:
-        
         app.stock_symbol = request.form['stock_symbol']
-        
-        mydata = requests.get("https://www.quandl.com/api/v3/datasets/WIKI/"+app.stock_symbol+".json?rows=30&column_index=4") #pulling col=4 (Closing price)
+        #app.closing_price = request.form['closing_price']
+        #app.volume = request.form['volume']
+        if request.form.get("closing_price"):
+            app.closing_price = request.form['closing_price']
+        else:
+            app.closing_price = False
+
+        if request.form.get("volume"):
+            app.volume = request.form['volume']
+        else:
+            app.volume = False
+
+        if app.volume == False and app.closing_price == False:
+            app.script = ''
+            app.div = ''
+            
+            app.msg = 'You did not select any data (e.g., volume or closing price) to be displayed.'
+            return render_template('error_page.html', msg = app.msg)
+            
+        mydata = requests.get("https://www.quandl.com/api/v3/datasets/WIKI/"+app.stock_symbol+".json?rows=30")
         
         if 'quandl_error' in mydata.json():
             app.script = ''
             app.div = ''
             
-            return render_template('error_page.html', stock_symbol = app.stock_symbol)
+            app.msg = 'The entered stock symbol "' + app.stock_symbol + '" is invalid.'
+            return render_template('error_page.html', msg = app.msg)
+            
         else:
             df = pd.DataFrame(mydata.json())
             dates=pd.to_datetime(np.array(df.ix['data'][0])[:,0])
-            closing_prices = (np.array(df.ix['data'][0])[:,1]).astype(float)
-            app.stock_name = mydata.json()['dataset']['name']
+            closing_prices = (np.array(df.ix['data'][0])[:,4]).astype(float)
+            volume = (np.array(df.ix['data'][0])[:,5]).astype(float)
+            factor=10**(len(str(int(volume[0]/closing_prices[0]))))
+            volume = volume/factor
+            app.stock_name = df['dataset']['name']
             extra_text_index = app.stock_name.find("Prices, Dividends, Splits and Trading Volume")
             if extra_text_index != -1:
                 app.stock_name = app.stock_name[0:extra_text_index-1]
@@ -54,9 +77,12 @@ def index():
             # select the tools we want
             TOOLS="pan,wheel_zoom,box_zoom,reset,save"
         
-            p1 = figure(tools=TOOLS, plot_width=500, plot_height=500, x_axis_type="datetime", x_axis_label='Date', y_axis_label="Price ($)")
-            p1.line(dates, closing_prices,line_width=3)
-            p1.circle(dates, closing_prices, fill_color="red", size=6)
+            p1 = figure(tools=TOOLS, plot_width=500, plot_height=500, x_axis_type="datetime", x_axis_label='Date')
+            if app.closing_price != False:
+                p1.line(dates, closing_prices,line_width=2, color="blue", legend="closing price")
+            if app.volume != False:
+                p1.line(dates, volume,line_width=2, color="red",legend="volume/"+str(factor),)
+            #p1.circle(dates, closing_prices, fill_color="red", size=6)
         
             plots = {'Red': p1}
         
@@ -70,7 +96,7 @@ def index():
             
 @app.route('/graph_page')
 def graph_page():
-    return render_template('graph.html',stock_symbol = app.stock_symbol, stock_name = app.stock_name, scr = Markup(app.script), diiv = Markup(app.div))
+    return render_template('graph.html',stock_symbol = app.stock_symbol, closing_price = app.closing_price, volume = app.volume, stock_name = app.stock_name, scr = Markup(app.script), diiv = Markup(app.div))
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
